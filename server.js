774a02ef1f72671545fad7e49e4e057c2ebf1cf7
martnga <mtnjoroge@gmail.com>
2011-01-01 06:10:52 +0300

    File serving

--- server.js
+++ server.js
@@ -1,6 +1,53 @@
+// Require Modules
 var http = require('http');
-http.createServer(function(req, res){
-	res.writeHead(200, {'Cotent-Type': 'text/plain'});
-	res.end('Hello World\n');
-}).listen(1337, '127.0.0.1');
-console.log('Server is running at http://127.0.0.1:1337/');
+var url = require('url');
+var path = require('path');
+var fs = require('fs');
+
+//Array of Mime Types
+var mimeTypes = {
+	"html" : "text/jpeg",
+	"jpeg": "image/jpeg",
+	"jpg": "image/jpeg",
+	"png": "image/png",
+	"js": "text/javascript",
+	"css": "text/css"
+};
+
+//Create Server
+http.createServer(function(){
+	var uri = url.parse(req.url).pathname;
+	var fileName = path.join(process.cwd(), unescape(uri));
+	console.log('Loading ' + uri);
+	var stats;
+	
+	try{
+		stats = fs.lstatSync(fileName);
+	}catch{
+		res.writeHead(404, {'Content-type': 'text/plain'});
+	  res.write('404 Not Found\n');
+	  res.end();
+	  return;
+	
+		}
+	
+		//Check if file/Directory
+	  if(stats.isFile()){
+    var mimeType = mimeTypes[path.extname(fileName).split(".").reverse()[0]];
+ res.writeHead(200, {'Content-type': mimeType});
+ 
+ var fileStream = fs.createReadStream(fileName);
+ fileStream.pipe(res);
+}else if(stats.isDirectory()){
+  res.writeHead(302, {
+   'Location': 'index.html'
+  });
+  res.end();
+} else {
+  res.writeHead(500, {'Content-Type' : 'text/plain'});
+  res.write('500 Internal Error\n');
+  res.end();
+}
+		
+}).listen(3000);
+	
